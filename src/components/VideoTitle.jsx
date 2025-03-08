567952e5a6f98ceff2a31e07d5f443dbecacf8d2
Rohan Saksena <rohan30saksena@gmail.com>
2025-03-08 20:35:00 +0530

    video-title

--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function VideoTitle({title, overview}) {
   return (
-    <div className='pt-36 w-screen aspect-video pt-[20%] px-24 absolute bg-gradient-to-r from-black'>
+    <div className='w-screen aspect-video pt-[20%] px-24 absolute bg-gradient-to-r from-black'>
         <div>
             <h1 className='text-6xl text-white font-bold'>{title}</h1>
             <p className='py-6 text-lg text-white w-1/4'>{overview}</p>
